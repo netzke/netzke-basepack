20134cc1f96c869d366134bbd6662e4d0326e303
Sergei Kozlov <s.n.kozlov@gmail.com>
2008-12-27 15:54:38 -0600

    a file forgotten

---
+++ lib/netzke-basepack.rb
@@ -0,0 +1,14 @@
+# External dependencies
+require 'netzke-core'
+require 'searchlogic'
+
+require 'netzke/ar_ext'
+
+%w{ models }.each do |dir|
+  path = File.join(File.dirname(__FILE__), 'app', dir)
+  $LOAD_PATH << path
+  ActiveSupport::Dependencies.load_paths << path
+  ActiveSupport::Dependencies.load_once_paths.delete(path)
+end
+
+Netzke::Base.config[:javascripts] << "#{File.dirname(__FILE__)}/../javascripts/basepack.js"
