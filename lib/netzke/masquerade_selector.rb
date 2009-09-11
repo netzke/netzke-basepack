45fd470f473683e1d63b7b16128eb79a8e199979
Sergei Kozlov <s.n.kozlov@gmail.com>
2009-09-10 21:47:32 -0500

    Fix: MasqueradeSelector widget added.

---
+++ lib/netzke/masquerade_selector.rb
@@ -0,0 +1,53 @@
+module Netzke
+  class MasqueradeSelector < TabPanel
+    
+    def items
+      @items ||= [{
+          :name              => "roles",
+          :active            => true,
+          :widget_class_name => "GridPanel",
+          :data_class_name   => 'Role',
+          :columns           => [:id, :name],
+          :ext_config => {
+            :header        => false,
+            :bbar => ['search']
+          }
+        },{
+          :name                 => "users",
+          :preloaded            => true,
+          :widget_class_name    => "GridPanel", 
+          :data_class_name      => 'User', 
+          :ext_config           => {
+            :header        => false,
+            :rows_per_page => 10,
+            :bbar => ['search']
+          },
+          :columns => [:id, :login]
+      }]
+    end
+
+    def self.js_extend_properties
+      {
+        :after_constructor => <<-END_OF_JAVASCRIPT.l,
+          function(){
+            this.items.each(function(tab){
+              tab.on('add', function(ct, cmp){
+                cmp.on('rowclick', this.rowclickHandler, this);
+              }, this);
+            }, this);
+          }
+        END_OF_JAVASCRIPT
+        
+        :rowclick_handler => <<-END_OF_JAVASCRIPT.l
+          function(grid, rowIndex, e){
+            var mode = grid.id.split("__").pop();
+            var normMode = mode === 'users' ? 'user' : 'role';
+            this.masquerade = {};
+            this.masquerade[normMode] = grid.store.getAt(rowIndex).get('id');
+          }
+        END_OF_JAVASCRIPT
+      }
+    end
+    
+  end
+end
