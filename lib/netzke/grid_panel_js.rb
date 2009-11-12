7e62267997eec5e1b5abbd30a555e67a2d7b042c
skozlov <sergei@writelesscode.com>
2009-11-12 17:19:25 -0600

    the renderer option in column config now can accept an inline function

--- lib/netzke/grid_panel_js.rb
+++ lib/netzke/grid_panel_js.rb
@@ -104,8 +104,16 @@ module Netzke
                 }
 
                 // set the renderer
-                var renderer = Ext.netzke.normalizedRenderer(c.renderer);
-                if (renderer != null) c.renderer = renderer;
+                if (c.renderer && c.renderer.match(/^\s*function\s*\(/)) {
+                  // if the renderer is an inline function - eval it
+                  eval("c.renderer = " + c.renderer);
+                } else {
+                  // othrewise it's a string representing the name of the renderer or an json-encoded array,
+                  // where the first parameter is the renderer's name, and the rest - parameters that should be
+                  // passed to the renderer at the moment of calling
+                  var renderer = Ext.netzke.normalizedRenderer(c.renderer);
+                  if (renderer != null) c.renderer = renderer;
+                }
                 
                 // add to the list
                 cmConfig.push(c);
