41fad96de9ae98451fc9d4ae0bed643cb2dd9f05
skozlov <sergei@writelesscode.com>
2010-06-21 19:28:03 +0200

    Fix: when used with Bundler, was crashing with the "uninitialized constant" exception

--- lib/netzke/active_record.rb
+++ lib/netzke/active_record.rb
@@ -1,10 +1,11 @@
-module Netzke::ActiveRecord
-  
-  # Extend ActiveRecord
-  ActiveRecord::Base.class_eval do
-    include ::Netzke::ActiveRecord::AssociationAttributes
-    include ::Netzke::ActiveRecord::Attributes
-    include ::Netzke::ActiveRecord::ComboboxOptions
-  end
-  
+require 'netzke/active_record/association_attributes'
+require 'netzke/active_record/attributes'
+require 'netzke/active_record/combobox_options'
+require 'netzke/active_record/data_accessor'
+
+# Extend ActiveRecord
+ActiveRecord::Base.class_eval do
+  include ::Netzke::ActiveRecord::AssociationAttributes
+  include ::Netzke::ActiveRecord::Attributes
+  include ::Netzke::ActiveRecord::ComboboxOptions
 end
