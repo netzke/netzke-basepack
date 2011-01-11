cfd51d36757c80427412f4cd5d8bb502c83d4357
Sergei Kozlov <sergei@playcode.nl>
2011-01-11 10:52:01 +0100

    Presets for searches

--- lib/netzke/basepack/new_search_panel.rb
+++ lib/netzke/basepack/new_search_panel.rb
@@ -37,6 +37,9 @@ module Netzke
       action :add_condition, :icon => :add
       action :serialize, :icon => :information
 
+      action :save_preset, :icon => :disk
+      action :delete_preset, :icon => :cross
+
       def default_query
         data_class.column_names.map do |c|
           column_type = data_class.columns_hash[c].type
@@ -47,7 +50,24 @@ module Netzke
 
       def configuration
         super.merge({
-          :bbar => [:add_condition.action, :clear_all.action, "-", :serialize.action]
+          :bbar => [:add_condition.action, :clear_all.action, "-", :serialize.action, "-",
+            "Presets:",
+            {
+              :xtype => "combo",
+              :fieldLabel => "Presets",
+              :triggerAction => "all",
+              :value => super[:load_last_preset] && last_preset.try(:fetch, "name"),
+              :store => (state[:presets] || []).map{ |s| [s["query"], s["name"]] },
+              :id => "presets-combo",
+              :listeners => {:before_select => {
+                :fn => "function(combo, record){
+                  var form = Ext.getCmp('#{global_id}');
+                  form.removeAll();
+                  form.buildFormFromQuery(record.data.field1);
+                }".l
+              }}
+            }, :save_preset.action, :delete_preset.action
+          ]
         })
       end
 
@@ -59,44 +79,31 @@ module Netzke
         super.merge(
           :attrs => data_class.column_names,
           :attrs_hash => data_class.column_names.inject({}){ |hsh,c| hsh.merge(c => data_class.columns_hash[c].type) },
-          :query => config[:query] || default_query
+          :query => (config[:load_last_preset] ? last_preset.try(:fetch, "query") : config[:query]) || default_query
         )
+      end
+
+      def last_preset
+        (state[:presets] || []).last
       end
 
       js_method :init_component, <<-JS
         function(){
           Netzke.classes.Basepack.NewSearchPanel.superclass.initComponent.call(this);
-
-          Ext.each(this.query, function(f){
-            this.add(Ext.apply(f, {xtype: 'netzkebasepacknewsearchpanelconditionfield'}));
-          }, this);
-
-          // this.add({
-          //   xtype: 'compositefield',
-          //   hideLabel: true,
-          //   items: [{
-          //     // work-around button display problem
-          //     xtype: 'textfield',
-          //     hidden: true
-          //   },{
-          //     xtype: 'button',
-          //     // cls: 'x-btn-icon',
-          //     text: 'Add condition',
-          //     icon: "/images/icons/add.png",
-          //     handler: this.onAddField,
-          //     scope: this
-          //   },{
-          //     xtype: 'button',
-          //     // cls: 'x-btn-icon',
-          //     text: 'Clear all',
-          //     icon: "/images/icons/cross.png",
-          //     handler: this.onClearAll,
-          //     scope: this
-          //   }]
-          //
-          // });
+          this.buildFormFromQuery(this.query);
         }
       JS
+
+      js_method :build_form_from_query, <<-JS
+        // Will probably need to be performance-optimized in the future, as recreating the fields is expensive
+        function(query){
+          Ext.each(query, function(f){
+            this.add(Ext.apply(f, {xtype: 'netzkebasepacknewsearchpanelconditionfield'}));
+          }, this);
+          this.doLayout();
+        }
+      JS
+
 
       js_method :on_add_condition, <<-JS
         function(){
@@ -118,17 +125,38 @@ module Netzke
       JS
 
       js_method :get_query, <<-JS
-        function(){
+        function(all){
           var query = [];
           this.items.each(function(f){
-            if (f.valueIsSet()) {
-              query.push(f.buildValue());
+            if (f.valueIsSet() || all) {
+              var cond = f.buildValue();
+              if (all) {cond.attrType = f.attrType;}
+              query.push(cond);
             }
           });
           return query;
         }
       JS
 
+      endpoint :save_preset do |params|
+        saved_searches = state[:presets] || []
+        existing = saved_searches.detect{ |s| s["name"] == params[:name] }
+        query = ActiveSupport::JSON.decode(params[:query])
+        if existing
+          existing["query"].replace(query)
+        else
+          saved_searches << {"name" => params[:name], "query" => query}
+        end
+        update_state(:presets, saved_searches)
+        {:feedback => "Preset successfully saved"} # TODO: I18n
+      end
+
+      endpoint :delete_preset do |params|
+        saved_searches = state[:presets]
+        saved_searches.delete_if{ |s| s["name"] == params[:name] }
+        update_state(:presets, saved_searches)
+        {:feedback => "Preset successfully deleted"}
+      end
 
     end
   end
--- test/rails_app/app/components/book_search_panel.rb
+++ test/rails_app/app/components/book_search_panel.rb
@@ -1,5 +1,5 @@
 class BookSearchPanel < Netzke::Basepack::NewSearchPanel
   def default_config
-    super.merge(:model => "Book")
+    super.merge(:model => "Book", :persistence => true)
   end
 end
