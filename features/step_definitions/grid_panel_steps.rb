adabe1546d921357fcbf0a66d84a2868a1739154
Sergei Kozlov <sergei@playcode.nl>
2011-02-11 16:54:30 +0100

    Fixing GridPanel store operations (after upgrading Rails to 3.0.4)

--- features/step_definitions/grid_panel_steps.rb
+++ features/step_definitions/grid_panel_steps.rb
@@ -101,3 +101,12 @@ When /^I stop editing the grid$/ do
     grid.stopEditing();
   JS
 end
+
+When /^I reload the grid$/ do
+  page.driver.browser.execute_script <<-JS
+    var components = [];
+    for (var cmp in Netzke.page) { components.push(cmp); }
+    var grid = Netzke.page[components[0]];
+    grid.getStore().reload();
+  JS
+end
