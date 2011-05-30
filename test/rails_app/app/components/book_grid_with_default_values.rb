9ffb73b76440e1487a0613487a228a6f09bd210f
Sergei Kozlov <sergei@playcode.nl>
2011-05-30 17:08:56 +0400

    WIP (fixing default values in grid)

--- test/rails_app/app/components/book_grid_with_default_values.rb
+++ test/rails_app/app/components/book_grid_with_default_values.rb
@@ -4,7 +4,7 @@ class BookGridWithDefaultValues < Netzke::Basepack::GridPanel
   def default_config
     super.merge(
       :model => "Book",
-      :columns => [{:name => 'author__last_name', :default_value => Author.first.id}, {:name => 'title', :default_value => "Lolita"}, {:name => 'exemplars', :default_value => 100}, {:name => 'digitized', :default_value => true}]
+      :columns => [{:name => 'title', :default_value => "Lolita"}, {:name => 'author__last_name', :default_value => Author.first.id}, {:name => 'exemplars', :default_value => 100}, {:name => 'digitized', :default_value => true}]
     )
   end
 
