b7150912dbd47b5f757d7290bf615d61e6685ff0
Sergei Kozlov <sergei@playcode.nl>
2011-08-18 09:40:39 +0200

    Using public/extjs, not public/extjs4 in test_app

--- test/rails_app/config/initializers/netzke.rb
+++ test/rails_app/config/initializers/netzke.rb
@@ -3,7 +3,7 @@ Netzke::Basepack.setup do |config|
 end
 
 Netzke::Core.setup do |config|
-  config.ext_uri = "/extjs4"
+  # config.ext_uri = "/extjs4"
   # config.ext3_compat_uri = "/extjs-compatibility"
   config.js_direct_max_retries = 0
 end
