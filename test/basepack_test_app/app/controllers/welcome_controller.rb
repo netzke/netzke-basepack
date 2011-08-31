bd2577aea4c301db6d9bb3c1c7d3592f44ae9055
Sergei Kozlov <sergei@playcode.nl>
2011-08-31 19:20:48 +0200

    A fix in the test components list

--- test/basepack_test_app/app/controllers/welcome_controller.rb
+++ test/basepack_test_app/app/controllers/welcome_controller.rb
@@ -5,5 +5,5 @@ class WelcomeController < ApplicationController
   end
 
   # Build components list from basepack_test_app/app/components
-  @@component_list ||= Dir.glob(File.expand_path('../../components/*.rb', __FILE__)).map{ |name| File.basename(name, ".rb") }.map(&:classify)
+  @@component_list ||= Dir.glob(File.expand_path('../../components/*.rb', __FILE__)).map{ |name| File.basename(name, ".rb") }.map(&:camelize)
 end
