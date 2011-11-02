f5bec2d7945b1ecf3e7ff32834d2763565b9be89
Sergei Kozlov <sergei@playcode.nl>
2011-11-02 15:11:19 +0200

    Minor impr to seeds.rb

--- test/basepack_test_app/db/seeds.rb
+++ test/basepack_test_app/db/seeds.rb
@@ -7,12 +7,14 @@
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
 Role.delete_all
+puts "Creating roles..."
 Role.create([
   {:name => "writer"},
   {:name => "reader"}
 ])
 
 User.delete_all
+puts "Creating users..."
 User.create([
   {:first_name => "Mark", :last_name => "Twain", :role => Role.find_by_name("writer")},
   {:first_name => "Carlos", :last_name => "Castaneda", :role => Role.find_by_name("writer")},
@@ -21,6 +23,7 @@ User.create([
 ])
 
 Author.delete_all
+puts "Creating authors..."
 Author.create([
   {:first_name => "Carlos", :last_name => "Castaneda"},
   {:first_name => "Herman", :last_name => "Hesse"}
@@ -30,6 +33,7 @@ Book.delete_all
 
 hesse = Author.find_by_last_name("Hesse")
 castaneda = Author.find_by_last_name("Castaneda")
+puts "Creating books..."
 Book.create([
   {:title => "Journey to Ixtlan", :author => castaneda},
   {:title => "The Tales of Power", :author => castaneda},
