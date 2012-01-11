ec8ba532eaa10f8d3aa6ced4193e0bece32acca8
Paul Schyska <pschyska@googlemail.com>
2012-01-11 15:53:45 +0100

    Config file and skript for travis ci

---
+++ config/ci/before-travis.sh
@@ -0,0 +1,19 @@
+# Start xvfb in preparation for cucumber
+sh -e /etc/init.d/xvfb start
+
+# fetch extjs
+wget http://extjs.cachefly.net/ext-4.0.2a-gpl.zip
+unzip -d test/basepack_test_app/public/ -n ext-4.0.2a-gpl.zip
+mv test/basepack_test_app/public/ext-4.0.2a test/basepack_test_app/public/extjs
+
+# clone netzke-core and netzke-persistence gems into test project
+mkdir -p test/basepack_test_app/vendor/gems
+cd test/basepack_test_app/vendor/gems
+git clone git://github.com/skozlov/netzke-core.git
+cd netzke-core
+git checkout tags/v0.7.4
+cd ..
+git clone git://github.com/skozlov/netzke-persistence.git
+cd netzke-persistence
+git checkout tags/v0.1.0
+cd ../../..
