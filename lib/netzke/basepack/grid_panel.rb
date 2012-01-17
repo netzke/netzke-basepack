7acdfbbe43b773d2090892d9a5b5baef1789bed9
Sergei Kozlov <sergei@playcode.nl>
2012-01-18 00:11:02 +0700

    Fix displaying dates in grids (still have to figure out about default formats)

--- lib/netzke/basepack/grid_panel.rb
+++ lib/netzke/basepack/grid_panel.rb
@@ -75,7 +75,7 @@ module Netzke
     #       }
     #     end
     #
-    # * +format+ - the format to display data when the column type is set to :datetime. Defaults to 'Y-m-d g:i:s'.
+    # * +format+ - the format to display data in case of date and datetime columns, e.g. 'Y-m-d g:i:s'.
     #
     # Besides these options, a column can receive any meaningful config option understood by Ext.grid.column.Column.
     #
