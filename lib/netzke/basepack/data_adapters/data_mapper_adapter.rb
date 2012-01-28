03e4058954f471e37531fb141643582761fcd638
Paul Schyska <pschyska@googlemail.com>
2012-01-28 02:37:03 +0100

    removed debugging output

--- lib/netzke/basepack/data_adapters/data_mapper_adapter.rb
+++ lib/netzke/basepack/data_adapters/data_mapper_adapter.rb
@@ -80,10 +80,6 @@ module Netzke::Basepack::DataAdapters
         column_names=@model_class.column_names
         column_name=c[:name]
       end
-      if c[:name] == "in_abundance"
-        p c, column_name, column_names
-
-      end
       !column_names.include? column_name
     end
 
