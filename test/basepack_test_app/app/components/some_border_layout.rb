99a2b3f3dac88b2ea253a05f99560e58c9f44aae
Sergei Kozlov <sergei@playcode.nl>
2012-04-11 11:06:13 +0545

    Update config syntax in SomeBorderLayout

--- test/basepack_test_app/app/components/some_border_layout.rb
+++ test/basepack_test_app/app/components/some_border_layout.rb
@@ -1,12 +1,8 @@
 class SomeBorderLayout < Netzke::Basepack::BorderLayoutPanel
-  def configuration
-    super.merge(
-      :items => [
-        {:title => "Who", :class_name => "Basepack::GridPanel", :region => :center, :model => "User", :name => :user_grid},
-        {:title => "Item Two", :class_name => "Basepack::GridPanel", :region => :west, :width => 500, :split => true, :collapsible => true, :model => "Role", :name => :role_grid}
-      ]
-    )
-  end
+  items [
+    {:title => "Who", :class_name => "Netzke::Basepack::GridPanel", :region => :center, :model => "User", :name => :user_grid},
+    {:title => "Item Two", :class_name => "Netzke::Basepack::GridPanel", :region => :west, :width => 500, :split => true, :collapsible => true, :model => "Role", :name => :role_grid}
+  ]
 
   js_property :bbar, [:update_center_region.action, :update_west_region.action]
 
@@ -24,5 +20,4 @@ class SomeBorderLayout < Netzke::Basepack::BorderLayoutPanel
       this.child('component[name="role_grid"]').body.update('Updated Center Region Content');
     }
   JS
-
 end
