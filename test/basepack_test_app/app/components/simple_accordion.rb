ade019e981c119ac1b5b79370cdc87999a437f20
Sergei Kozlov <sergei@playcode.nl>
2012-04-14 17:19:19 +0545

    Fix SimpleAccordion

--- test/basepack_test_app/app/components/simple_accordion.rb
+++ test/basepack_test_app/app/components/simple_accordion.rb
@@ -1,11 +1,14 @@
 class SimpleAccordion < Netzke::Basepack::AccordionPanel
-  config  :items => [{
-            :html => "I'm a simple Ext.Panel",
-            :title => "Panel One"
-          },{
-            :class_name => "SimplePanel",
-            :update_text => "Update for Panel Two",
-            :title => "Panel Two",
-            :lazy_loading => true
-          }]
+  def configure
+    super
+    config.items = [{
+      :html => "I'm a simple Ext.Panel",
+      :title => "Panel One"
+    },{
+      :class_name => "SimplePanel",
+      :update_text => "Update for Panel Two",
+      :title => "Panel Two",
+      :lazy_loading => true
+    }]
+  end
 end
