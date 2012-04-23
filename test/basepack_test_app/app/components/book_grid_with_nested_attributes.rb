887aa77cd366752d913990aaeec13c7cc029d69f
Sergei Kozlov <sergei@playcode.nl>
2012-04-23 12:04:55 +0545

    Fix another test

--- test/basepack_test_app/app/components/book_grid_with_nested_attributes.rb
+++ test/basepack_test_app/app/components/book_grid_with_nested_attributes.rb
@@ -1,13 +1,14 @@
 class BookGridWithNestedAttributes < Netzke::Basepack::GridPanel
   model "Book"
 
-  def configuration
+  def configure
     super
-    @config[:columns] = [:title, {:name => :author__first_name, :nested_attribute => true}, {:name => :author__last_name, :nested_attribute => true}]
+    config.columns = [:title, {:name => :author__first_name, :nested_attribute => true}, {:name => :author__last_name, :nested_attribute => true}]
   end
 
-  def default_fields_for_forms
-    [{:name => :title}, {:name => :author__first_name, :nested_attribute => true}, {:name => :author__last_name, :nested_attribute => true}]
+  # Override edit_window component in order to provide a custom list of fields for the form
+  def edit_window_component(c)
+    super
+    c.form_config.items = [{:name => :title}, {:name => :author__first_name, :nested_attribute => true}, {:name => :author__last_name, :nested_attribute => true}]
   end
-
 end
