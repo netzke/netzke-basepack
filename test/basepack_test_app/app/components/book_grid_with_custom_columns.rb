90e9162386cc9cc5a38218c1bc48722de29c7d74
Denis Gorin <nmcoder@gmail.com>
2012-08-11 20:47:01 +0200

    Get rid of `def columns` in BookGridWithCustomColumns

--- test/basepack_test_app/app/components/book_grid_with_custom_columns.rb
+++ test/basepack_test_app/app/components/book_grid_with_custom_columns.rb
@@ -1,6 +1,4 @@
 class BookGridWithCustomColumns < Netzke::Basepack::GridPanel
-  model "Book"
-
   column :author__first_name do |c|
     c.renderer = :my_renderer
   end
@@ -28,24 +26,10 @@ class BookGridWithCustomColumns < Netzke::Basepack::GridPanel
     c.renderer = "function(v){return ['', 'Good', 'Average', 'Poor'][v];}"
   end
 
-  def columns
-    [ :author__first_name, :author__last_name, :author__name, :title, :digitized, :rating, :exemplars, :updated_at ]
+  def configure(c)
+    c.model = "Book"
+    c.columns = [ :author__first_name, :author__last_name, :author__name, :title, :digitized, :rating, :exemplars, :updated_at ]
   end
-
-  #column :author__first_name, :renderer => :my_renderer
-  #column :author__last_name, :renderer => :uppercase
-  #column :author__name, :flex => 1, :text => "Author"
-  #column :title, :flex => 1
-  #column :digitized
-  #column :rating,
-    #:editor => {
-      #:trigger_action => :all,
-      #:xtype => :combo,
-      #:store => [[1, "Good"], [2, "Average"], [3, "Poor"]]
-    #},
-    #:renderer => "function(v){return ['', 'Good', 'Average', 'Poor'][v];}"
-  #column :exemplars
-  #column :updated_at
 
   js_configure do |c|
     c.my_renderer = <<-JS
