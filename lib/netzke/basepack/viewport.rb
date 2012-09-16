4d2d9560fe52ce581e6e89d675778b378653b7fc
Denis Gorin <nmcoder@gmail.com>
2012-09-16 10:58:13 +0300

    Add forgotten namespace to Viewport

--- lib/netzke/basepack/viewport.rb
+++ lib/netzke/basepack/viewport.rb
@@ -1,12 +1,16 @@
-class Viewport < Netzke::Base
-  js_configure do |c|
-    c.extend = "Ext.container.Viewport"
-  end
+module Netzke
+  module Basepack
+    class Viewport < Netzke::Base
+      js_configure do |c|
+        c.extend = "Ext.container.Viewport"
+      end
 
-  # In Ext 4.1 calling `render` on a viewport causes an error:
-  #
-  #   TypeError: protoEl is null
-  def js_component_render
-    ""
+      # In Ext 4.1 calling `render` on a viewport causes an error:
+      #
+      #   TypeError: protoEl is null
+      def js_component_render
+        ""
+      end
+    end
   end
 end
