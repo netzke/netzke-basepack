be5b44d9477a61b495849eef185660f38776735c
nomadcoder <nmcoder@gmail.com>
2013-01-10 12:37:33 +0700

    Add ActionColumn
    Moved over from Communitypack and documented.

---
+++ test/basepack_test_app/app/components/grid_with_action_column.rb
@@ -0,0 +1,32 @@
+class GridWithActionColumn < Netzke::Basepack::Grid
+  include Netzke::Basepack::ActionColumn
+
+  def configure(c)
+    c.model = "Book"
+    super
+  end
+
+  def columns
+    super + [:basic_actions, :extra_actions]
+  end
+
+  column :basic_actions do |c|
+    c.type = :action
+    c.actions = [{name: :delete_row, tooltip: "Delete row", icon: :delete}]
+  end
+
+  # Just for illustation (no handlers assigned)
+  column :extra_actions do |c|
+    c.type = :action
+    c.actions = [:error, :information]
+  end
+
+  js_configure do |c|
+    c.on_delete_row = <<-JS
+      function(record){
+        this.getSelectionModel().select(record);
+        this.onDel();
+      }
+    JS
+  end
+end
