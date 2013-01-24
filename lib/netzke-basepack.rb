7be04fd0f297e8a9d16e7f1a4f167c77b08f344e
denispeplin <denis.peplin@gmail.com>
2013-01-24 14:44:22 +0400

    Add Russian translation from '0-5' branch and update it.

--- lib/netzke-basepack.rb
+++ lib/netzke-basepack.rb
@@ -13,6 +13,7 @@ module Netzke
 
       I18n.load_path << File.dirname(__FILE__) + '/../locales/en.yml'
       I18n.load_path << File.dirname(__FILE__) + '/../locales/de.yml'
+      I18n.load_path << File.dirname(__FILE__) + '/../locales/ru.yml'
 
       config.after_initialize do
         Netzke::Core.external_ext_css << "#{Netzke::Core.ext_uri}/examples/ux/grid/css/RangeMenu"
