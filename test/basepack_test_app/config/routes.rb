bb5cb5135a301af902534e0a5508bcce3b7224d6
Max Gorin <nmcoder@gmail.com>
2013-04-10 16:58:11 +0545

    Declare Netzke routes early in routes.rb
    To comply with recent update to Netzke Core.

--- test/basepack_test_app/config/routes.rb
+++ test/basepack_test_app/config/routes.rb
@@ -61,10 +61,11 @@ RailsApp::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
 
+  netzke
+
   match 'components/:component' => 'components#index', :as => "components"
   match 'components/embedded/:action' => 'components', :as => "embedded_components"
 
   match ':controller(/:action(/:id(.:format)))'
 
-  netzke
 end
