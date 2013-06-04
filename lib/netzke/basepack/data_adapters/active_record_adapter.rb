7c7394ba8299c28ae2ba2cdda6ffe6a7ed9632c4
phgrey <phgrey@gmail.com>
2013-06-04 15:33:35 +0300

    changes for has_one attribute on saving

--- lib/netzke/basepack/data_adapters/active_record_adapter.rb
+++ lib/netzke/basepack/data_adapters/active_record_adapter.rb
@@ -261,17 +261,19 @@ module Netzke::Basepack::DataAdapters
             # search for association and assign it to r
             assoc = @model_class.reflect_on_association(split.first.to_sym)
             if assoc
-              if (assoc.macro == :has_one) && attribute_mass_assignable?(assoc.foreign_key, role)
-                #here we just set the new new relationship, we
-                # do not change nested attributes here
-                assoc_instance = begin
-                  assoc.klass.find v
-                rescue ActiveRecord::RecordNotFound
-                  nil
+              if assoc.macro == :has_one
+                if attribute_mass_assignable?(split.first, role) || attribute_mass_assignable?(assoc.foreign_key, role)
+                  #here we just set the new new relationship, we
+                  # do not change nested attributes here
+                  assoc_instance = begin
+                    assoc.klass.find(v)
+                  rescue ActiveRecord::RecordNotFound
+                    nil
+                  end
+                  r.send "#{assoc.name}=", assoc_instance
+                  #this is excess, at least for checked examples
+                  r.save
                 end
-                r.send "#{assoc.name}=", assoc_instance
-                #this is excess, at least for checked examples
-                r.save # what should we do when this fails?..
               else
 
                 # set the foreign key to the passed value
