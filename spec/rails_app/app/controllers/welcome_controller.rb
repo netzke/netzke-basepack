cfdf8285ae9cc2de4abb86c8c9e8d75e19110945
Max Gorin <gorinme@gmail.com>
2013-12-01 14:11:52 +0700

    Some clean-up

--- spec/rails_app/app/controllers/welcome_controller.rb
+++ spec/rails_app/app/controllers/welcome_controller.rb
@@ -4,6 +4,6 @@ class WelcomeController < ApplicationController
     @components = @@component_list
   end
 
-  # Build components list from basepack_test_app/app/components
+  # Build components list from app/components
   @@component_list ||= Dir.glob(File.expand_path('../../components/*.rb', __FILE__)).map{ |name| File.basename(name, ".rb") }.map(&:camelize)
 end
