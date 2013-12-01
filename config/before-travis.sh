4de788a0631312479e0f0c80b9a35d7407a0b3ce
Max Gorin <gorinme@gmail.com>
2013-12-01 14:59:07 +0700

    Another attempt to fix travis build

--- config/before-travis.sh
+++ config/before-travis.sh
@@ -3,5 +3,5 @@ sh -e /etc/init.d/xvfb start
 
 # fetch extjs
 wget http://cdn.sencha.com/ext/gpl/ext-4.2.1-gpl.zip
-unzip -q -d spec/rails_app/public/ -n ext-4.2.1-gpl.zip
-mv spec/rails_app/public/ext-4.2.1 spec/rails_app/public/extjs
+unzip -q ext-4.2.1-gpl.zip
+mv extjs-4.2.1 spec/rails_app/public/extjs
