d70cfb6d5721332b304e1870c3398aebd9866faf
Max Gorin <gorinme@gmail.com>
2013-12-01 14:22:55 +0700

    Update travis config

--- config/before-travis.sh
+++ config/before-travis.sh
@@ -2,6 +2,6 @@
 sh -e /etc/init.d/xvfb start
 
 # fetch extjs
-wget http://cdn.sencha.io/ext-4.1.1a-gpl.zip
-unzip -q -d spec/rails_app/public/ -n ext-4.1.1a-gpl.zip
-mv spec/rails_app/public/ext-4.1.1a spec/rails_app/public/extjs
+wget http://cdn.sencha.com/ext/gpl/ext-4.2.1-gpl.zip
+unzip -q -d spec/rails_app/public/ -n ext-4.2.1-gpl.zip
+mv spec/rails_app/public/ext-4.2.1 spec/rails_app/public/extjs
