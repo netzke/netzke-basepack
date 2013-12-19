9ce9d92abdd7638d3b9d4af119a0fa90fed38e16
Max Gorin <gorinme@gmail.com>
2013-12-19 09:58:16 +0700

    Clean up no longer used fixtures

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -30,16 +30,6 @@ RSpec.configure do |config|
   # config.mock_with :flexmock
   # config.mock_with :rr
 
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.use_transactional_fixtures = false # we are cleaning up ourselves with DatabaseCleaner
-  config.global_fixtures = :all
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  # config.use_transactional_fixtures = true
-
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
