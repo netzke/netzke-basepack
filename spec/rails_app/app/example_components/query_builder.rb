8c10f879731c707d89dc454c3c7237cd35ef7168
Max Gorin <gorinme@gmail.com>
2014-02-08 21:23:06 +0700

    Remove a few outdated example components

--- spec/rails_app/app/example_components/query_builder.rb
+++
@@ -1,7 +0,0 @@
-class QueryBuilder < Netzke::Basepack::GridLib::QueryBuilder
-  def configure(c)
-    super
-    c.model = "Book"
-    c.fields = [{name: :title, field_label: "Title"}, {name: :author__name, field_label: "Author"}, {name: :digitized, field_label: "Digitized"}]
-  end
-end
