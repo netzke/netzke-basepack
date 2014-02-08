57a94d9c153dc6b468f4c6813635fcb1d219e5d8
Max Gorin <gorinme@gmail.com>
2014-02-08 16:22:54 +0700

    Keep version in Basepack::VERSION string
    Conflicts:
    	lib/netzke/basepack/version.rb

--- lib/netzke/basepack/version.rb
+++ lib/netzke/basepack/version.rb
@@ -1,12 +1,5 @@
 module Netzke
   module Basepack
-    module Version
-      MAJOR = 0
-      MINOR = 9
-      PATCH = 0
-      PRE   = 'rc1'
-
-      STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
-    end
+    VERSION = "0.9.0"
   end
 end
