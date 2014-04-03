39be561af0428bcb3e27f05b31dba7a10a498780
Max Gorin <gorinme@gmail.com>
2014-04-03 12:39:15 +0545

    Fix RelationExtensions

--- lib/netzke/basepack/active_record/relation_extensions.rb
+++ lib/netzke/basepack/active_record/relation_extensions.rb
@@ -4,18 +4,16 @@ module Netzke
       module RelationExtensions
         def extend_with(*params)
           scope = params.shift
-          case scope.class.name
-          when "Symbol" # model's scope
+          case scope
+          when Symbol # model's scope
             self.send(scope, *params)
-          when "String" # SQL query or SQL query with params (e.g. ["created_at < ?", 1.day.ago])
+          when String # SQL query or SQL query with params (e.g. ["created_at < ?", 1.day.ago])
             params.empty? ? self.where(scope) : self.where([scope, *params])
-          when "Array"
+          when Array
             self.extend_with(*scope)
-          when "Hash"  # conditions hash
+          when Hash  # conditions hash
             self.where(scope)
-          when "ActiveSupport::HashWithIndifferentAccess" # conditions hash
-            self.where(scope)
-          when "Proc"   # receives a relation, must return a relation
+          when Proc  # receives a relation, must return a relation
             scope.call(self)
           else
             raise ArgumentError, "Wrong parameter type for ActiveRecord::Relation#extend_with"
