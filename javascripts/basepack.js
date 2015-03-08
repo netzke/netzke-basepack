e9c12a2e49d938feee3b9c5eb6ec789ace7540fa
Max Gorin <gorinme@gmail.com>
2015-03-08 20:00:14 +0700

    Patch race condition bug in Ext JS 5

--- javascripts/basepack.js
+++ javascripts/basepack.js
@@ -12,3 +12,17 @@ Ext.define('Ext.ux.form.TriCheckbox', {
   store: [[true, "Yes"], [false, "No"]],
   forceSelection: true
 });
+
+// Fix race condition with Ext JS 5.1.0 (while testing)
+// The error was: "Ext.EventObject is undefined"
+// Looks like Ext.EventObject is a legacy artifact in Ext JS 5, as it can be found only twice in the whole code base,
+// so, it probably gets removed in one of the next releases.
+Ext.override(Ext.view.BoundList, {
+  onHide: function() {
+    var inputEl = this.pickerField.inputEl.dom;
+    if (Ext.Element.getActiveElement() !== inputEl) {
+      inputEl.focus();
+    }
+    this.callParent(arguments);
+  },
+});
