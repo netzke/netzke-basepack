698eb1a1bc1a08b497c02cac4c13d01a19b1cdd4
Max Gorin <max@goodbitlabs.com>
2015-12-18 12:45:21 +0700

    Fix lockable form example

--- spec/rails_app/app/example_components/lockable_book_form.rb
+++ spec/rails_app/app/example_components/lockable_book_form.rb
@@ -1,12 +1,4 @@
 class LockableBookForm < BookForm
-  # Code from the future
-  # field :author__first_name do |c|
-  #   c.xtype = :compositefield
-  #   c.defaults = {:flex => 1}
-  #   c.field_label = "Author name (first, last)"
-  #   c.items = [{:name => :author__first_name, :nested_attribute => true}, {:name => :author__last_name, :nested_attribute => true}]
-  # end
-
   def configure(c)
     super
     c.mode = :lockable
